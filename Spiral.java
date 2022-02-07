720639ecf2ac3d3b45ad757964093cf2ca55fbeb
neelamvijayvargiya <98867683+neelamvijayvargiya@users.noreply.github.com>
2022-02-07 19:49:10 +0530

    Add files via upload

--- Spiral.java
+++ Spiral.java
@@ -1,106 +1,75 @@
 package io.sevenx.javaprograms;
-
+import java.util.Scanner;
 public class Spiral {
 	
-	 
-	static void printSpiralPattern(int size)  
-	{  
-	//create two variables row and col to traverse rows and columns  
-	int row = 0, col = 0;  
-	int boundary = size - 1;  
-	int sizeLeft = size - 1;  
-	int flag = 1;  
-
-	char move = 'r';  
-	//creating a 2D array for matrix  
-	int[][] matrix =new int [size][size];  
-	for (int i = 1; i < size * size + 1; i++)  
-	{  
-	    //assigning values  
-	    matrix[row][col] = i;  
-	 
-	    switch (move)  
-	    {  
-	    //if right, go right  
-	    case 'r':  
-	        col += 1;  
-	        break;  
-	    //if left, go left  
-	    case 'l':  
-	        col -= 1;  
-	        break;  
-	    //if up, go up  
-	    case 'u':  
-	        row -= 1;  
-	        break;  
-	    //if down, go down  
-	    case 'd':  
-	    	 row += 1;  
-	    	    break;  
-	    	}  
-	   
-	    	if (i == boundary)  
-	    	    {  
-	    	        
-	    	        boundary = boundary + sizeLeft;  
-	    	         
-	    	        if (flag != 2)  
-	    	        {  
-	    	        flag = 2;  
-	    	        }  
-	    	        else  
-	    	        {  
-	    	        flag = 1;  
-	    	        sizeLeft -= 1;  
-	    	        }  
-	    	       
-	    	        switch (move)  
-	    	        {  
-	    	      //if right, rotate to down  
-	    	        case 'r':  
-	    	            move = 'd';  
-	    	            break;  
-	    	        // if down, rotate to left  
-	    	        case 'd':  
-	    	            move = 'l';  
-	    	            break;  
-	    	        // if left, rotate to up  
-	    	        case 'l':  
-	    	            move = 'u';  
-	    	            break;  
-	    	        // if up, rotate to right  
-	    	        case 'u':  
-	    	            move = 'r';  
-	    	            break;  
-	    	        }  
-	    	    }  
-	    	    }  
-	    	//printing the spiral matrix or pattern  
-	    	//outer for loop for rows  
-	 for (row = 0; row < size; row++)  
-	    {  
-	        //inner for loop for columns  
-	        for (col = 0; col < size; col++)  
-	            {  
-	                int n = matrix[row][col];  
-	                if(n < 10)  
-	                    System.out.print(n +" ");  
-	                else  
-	                    System.out.print(n +" ");  
-	            }  
-	        System.out.println();  
-	    }  
-	}  
- 
-	public static void main(String args[])  
-	{  
-
-	int size = 5;  
-	System.out.println("Spiral Matrix or Pattern is: \n");  
-	   
-	printSpiralPattern(size);  
-	}  
-	}  
-	    	        
-
+		public static void main(String args[])
+	    {
+			System.out.println("Enter The Value For N :");
+			
+			Scanner sc = new Scanner(System.in);
+			
+			int n = sc.nextInt();
+			
+			int[][] spiral = new int[n][n];
+			
+			int value = 1;
+			
+			int minCol = 0;
+			
+			int maxCol = n-1;
+			
+			int minRow = 0;
+			
+			int maxRow = n-1;
+			
+			while (value <= n*n)
+			{
+				for (int i = minCol; i <= maxCol; i++)
+	            {
+	                spiral[minRow][i] = value;
+	                     
+	                value++;
+	            }
+	             
+	            for (int i = minRow+1; i <= maxRow; i++) 
+	            { 
+	                spiral[i][maxCol] = value; 
+	                 
+	                value++; 
+	            } 
+	             
+	            for (int i = maxCol-1; i >= minCol; i--)
+	            {
+	                spiral[maxRow][i] = value;
+	                             
+	                value++;
+	            }
+	             
+	            for (int i = maxRow-1; i >= minRow+1; i--) 
+	            {
+	                spiral[i][minCol] = value;
+	                 
+	                value++;
+	            }
+	             
+	            minCol++;
+	             
+	            minRow++;
+	            maxCol--;
+	             
+	            maxRow--;
+	        }
+	         
+	        for (int i = 0; i < spiral.length; i++)
+	        {
+	            for (int j = 0; j < spiral.length; j++)
+	            {
+	                System.out.print(spiral[i][j]+ "\t");
+	            }
+	             
+	            System.out.println();
+	        }
+	    }
+	}
+	
 
